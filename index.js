e20a88a8174c90c2bb9b7a192e87a5bde85b6ab5
Erica Wright <ericaewright@gmail.com>
2018-11-14 10:59:27 -0500

    convert thought bubbles to sprite sheet

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let initFunction = () => {
   let swatches = document.querySelectorAll('.color-swatch');
-  console.log('hello');
   
   let changeThoughtBubble = (e) => {
     for (let swatch of swatches) {
@@ -9,7 +8,8 @@ let initFunction = () => {
     
     let bubble = document.getElementById('thoughtBubble');
     let targetSwatch = e.target;
-    bubble.style.backgroundImage = 'url(./assets/thinking-bubble-' + targetSwatch.id + '.png)';
+    bubble.className = '';
+    bubble.classList.add(targetSwatch.id);
     targetSwatch.classList.add('selected');
   }
   
